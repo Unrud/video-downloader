e028700b34694d732e1bb864acf6aed031c1e821
Unrud <unrud@outlook.com>
2020-08-16 03:51:16 +0200

    Revert "Merge branch 'Kekun-ui-polish'"
    This reverts commit 9f2d3d2808b1217fc1dd92ddb2f15fb528a14e44, reversing
    changes made to 5b7923c5bf6f8402964a6281693897593bfc6f6e.

--- src/window.py
+++ src/window.py
@@ -20,7 +20,7 @@ import locale
 import math
 import os
 
-from gi.repository import GLib, Gtk, GdkPixbuf, GObject, Handy
+from gi.repository import GLib, Gtk, GdkPixbuf, GObject
 
 from video_downloader.util import bind_property
 
@@ -30,7 +30,7 @@ N_ = gettext.gettext
 
 
 @Gtk.Template(resource_path='/com/github/unrud/VideoDownloader/window.ui')
-class Window(Handy.ApplicationWindow):
+class Window(Gtk.ApplicationWindow):
     __gtype_name__ = 'VideoDownloaderWindow'
     error_buffer = Gtk.Template.Child()
     resolutions_store = Gtk.Template.Child()
@@ -39,11 +39,11 @@ class Window(Handy.ApplicationWindow):
     resolution_wdg = Gtk.Template.Child()
     main_stack_wdg = Gtk.Template.Child()
     audio_video_stack_wdg = Gtk.Template.Child()
-    audio_download_wdg = Gtk.Template.Child()
-    video_download_wdg = Gtk.Template.Child()
-    error_back_wdg = Gtk.Template.Child()
-    success_back_wdg = Gtk.Template.Child()
-    download_cancel_wdg = Gtk.Template.Child()
+    audio_video_switcher_wdg = Gtk.Template.Child()
+    download_wdg = Gtk.Template.Child()
+    back_wdg = Gtk.Template.Child()
+    cancel_wdg = Gtk.Template.Child()
+    title_wdg = Gtk.Template.Child()
     success_msg_wdg = Gtk.Template.Child()
     download_title_wdg = Gtk.Template.Child()
     download_progress_wdg = Gtk.Template.Child()
@@ -67,6 +67,7 @@ class Window(Handy.ApplicationWindow):
         bind_property(
             self.model, 'state', self.main_stack_wdg, 'visible-child-name',
             lambda s: {'cancel': 'download'}.get(s, s))
+        bind_property(self.model, 'state', func_a_to_b=self._update_header)
         bind_property(self.model, 'mode', self.audio_video_stack_wdg,
                       'visible-child-name', bi=True)
         bind_property(self.main_stack_wdg, 'visible-child-name',
@@ -182,23 +183,27 @@ class Window(Handy.ApplicationWindow):
             return True
         return False
 
+    def _update_header(self, state):
+        self.audio_video_switcher_wdg.set_visible(state == 'start')
+        self.download_wdg.set_visible(state == 'start')
+        self.back_wdg.set_visible(state in ['success', 'error'])
+        self.cancel_wdg.set_visible(state in ['download', 'cancel'])
+        self.title_wdg.set_visible(state != 'start')
+
     def _update_focus_and_default(self, _):
         state = self.main_stack_wdg.get_visible_child_name()
         mode = self.audio_video_stack_wdg.get_visible_child_name()
         if state == 'start':
+            self.download_wdg.grab_default()
             if mode == 'audio':
-                self.audio_download_wdg.grab_default()
                 self.audio_url_wdg.grab_focus()
             elif mode == 'video':
-                self.video_download_wdg.grab_default()
                 self.video_url_wdg.grab_focus()
             else:
                 assert False
-        elif state == 'download':
-            self.download_cancel_wdg.grab_focus()
-        elif state == 'error':
-            self.error_back_wdg.grab_focus()
-        elif state == 'success':
-            self.success_back_wdg.grab_focus()
+        elif state in ['download', 'cancel']:
+            self.cancel_wdg.grab_focus()
+        elif state in ['success', 'error']:
+            self.back_wdg.grab_focus()
         else:
             assert False
