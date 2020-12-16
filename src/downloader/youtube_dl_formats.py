a6271108e35af6e51743ad0d43f9fa8fef096a8b
Unrud <unrud@outlook.com>
2020-12-16 17:46:43 +0100

    Update copyright header

--- src/downloader/youtube_dl_formats.py
+++ src/downloader/youtube_dl_formats.py
@@ -1,19 +1,19 @@
-# downloader/youtube_dl_formats.py
+# Copyright (C) 2019 Unrud <unrud@outlook.com>
 #
-# Copyright 2019 Unrud <unrud@outlook.com>
+# This file is part of Video Downloader.
 #
-# This program is free software: you can redistribute it and/or modify
+# Video Downloader is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# Video Downloader is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with Video Downloader.  If not, see <http://www.gnu.org/licenses/>.
 
 import youtube_dl
 
