28e107baed7173e41431e4871dab92fb1ec47f4f
Unrud <unrud@outlook.com>
2021-03-29 02:25:54 +0200

    Don't translate release notes

---
+++ data/build-aux/meson/merge-xml.py
@@ -0,0 +1,20 @@
+import sys
+import xml.etree.ElementTree as ET
+
+
+class CommentTreeBuilder(ET.TreeBuilder):
+    def comment(self, data):
+        self.start(ET.Comment, {})
+        self.data(data)
+        self.end(ET.Comment)
+
+
+def make_xml_parser():
+    return ET.XMLParser(target=CommentTreeBuilder())
+
+
+_, main_path, insert_path, insert_xpath = sys.argv
+main_xml = ET.parse(main_path, parser=make_xml_parser())
+insert_xml = ET.parse(insert_path, parser=make_xml_parser())
+main_xml.find(insert_xpath).append(insert_xml.getroot())
+main_xml.write(sys.stdout.buffer, encoding='utf-8', xml_declaration=True)
