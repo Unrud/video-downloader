f6f5904b44db6b03c71f0eeab235e557767b11f3
Unrud <unrud@outlook.com>
2021-03-30 08:12:48 +0200

    Meson: Cosmetic changes

--- build-aux/meson/postinstall.py
+++ build-aux/meson/postinstall.py
@@ -1,9 +1,9 @@
 from os import environ, path
 from subprocess import call
 
-prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
-datadir = path.join(prefix, 'share')
 destdir = environ.get('DESTDIR', '')
+prefix = environ['MESON_INSTALL_PREFIX']
+datadir = path.join(prefix, 'share')
 
 # Package managers set this so we don't need to run
 if not destdir:
