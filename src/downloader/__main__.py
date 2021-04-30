8c7194c924cb926ae2d03d7a0276fa3afabbe865
Unrud <unrud@outlook.com>
2021-05-01 00:57:20 +0200

    More robust communication with downloader process
    * Close communications fds even when children of downloader process are still alive
      * Fix cancelling when using external downloader
    * Prevent rogue writes to stdout (or reads from stdin) that break communication

--- src/downloader/__main__.py
+++ src/downloader/__main__.py
@@ -15,16 +15,23 @@
 # You should have received a copy of the GNU General Public License
 # along with Video Downloader.  If not, see <http://www.gnu.org/licenses/>.
 
+import fcntl
 import functools
 import json
+import os
 import signal
 import sys
 
 
 class Handler:
+    def __init__(self, input_file, output_file):
+        self._in = input_file
+        self._out = output_file
+
     def _rpc(self, name, *args):
-        print(json.dumps({'method': name, 'args': args}), flush=True)
-        answer = json.loads(sys.stdin.readline())
+        print(json.dumps({'method': name, 'args': args}),
+              file=self._out, flush=True)
+        answer = json.loads(self._in.readline())
         return answer['result']
 
     def __getattr__(self, name):
@@ -34,7 +41,20 @@ class Handler:
 if __name__ == '__main__':
     # Exit gracefully on SIGTERM to allow cleanup code to run
     signal.signal(signal.SIGTERM, lambda *_: sys.exit(1))
-    handler = Handler()
+    # Duplicate stdin and stdout for exclusive usage with handler.
+    # The handler waits for stdin and stdout to be closed. The fds must not
+    # be closed before the process exits to avoid race.
+    input_file = os.fdopen(os.dup(sys.stdin.fileno()), 'r', closefd=False)
+    output_file = os.fdopen(os.dup(sys.stdout.fileno()), 'w', closefd=False)
+    # Prevent leaking the fds to children that might remain after this process
+    # exits
+    fcntl.fcntl(input_file, fcntl.F_SETFL, os.O_CLOEXEC)
+    fcntl.fcntl(output_file, fcntl.F_SETFL, os.O_CLOEXEC)
+    # Redirect stdin and stdout to /dev/null to prevent interferences
+    with open(os.devnull, 'r+') as devnull:
+        os.dup2(devnull.fileno(), sys.stdin.fileno())
+        os.dup2(devnull.fileno(), sys.stdout.fileno())
+    handler = Handler(input_file, output_file)
     try:
         import youtube_dl
 
