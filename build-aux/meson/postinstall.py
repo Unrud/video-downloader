274bd2216f8e67b42aa6e2d3d0745287df2f6735
Unrud <unrud@outlook.com>
2021-08-25 19:57:11 +0200

    Replace deprecates subprocess.call

--- build-aux/meson/postinstall.py
+++ build-aux/meson/postinstall.py
@@ -1,5 +1,5 @@
 from os import environ, path
-from subprocess import call
+from subprocess import run
 
 destdir = environ.get('DESTDIR', '')
 prefix = environ['MESON_INSTALL_PREFIX']
@@ -8,11 +8,11 @@ datadir = path.join(prefix, 'share')
 # Package managers set this so we don't need to run
 if not destdir:
     print('Updating icon cache...')
-    call(['gtk-update-icon-cache', '-qtf',
-          path.join(datadir, 'icons', 'hicolor')])
+    run(['gtk-update-icon-cache', '-qtf',
+         path.join(datadir, 'icons', 'hicolor')])
 
     print('Updating desktop database...')
-    call(['update-desktop-database', '-q', path.join(datadir, 'applications')])
+    run(['update-desktop-database', '-q', path.join(datadir, 'applications')])
 
     print('Compiling GSettings schemas...')
-    call(['glib-compile-schemas', path.join(datadir, 'glib-2.0', 'schemas')])
+    run(['glib-compile-schemas', path.join(datadir, 'glib-2.0', 'schemas')])
